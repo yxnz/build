0184204fba026f324bb1edf13f80563e3e883fa1
yxn <72302442+yxnz@users.noreply.github.com>
2024-06-30 10:55:36 -0400

    Deploy

---
+++ static/pRoELD6gIoltq_kwBkS1l/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/412-45c4e7ef7b9488cc.js","static/chunks/pages/index-eef106b58613af8b.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
